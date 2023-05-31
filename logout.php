3e1740dedb41066e7e8830674edbfa9ee3ca4b87
John Christian <104075692+xtian1227002@users.noreply.github.com>
2023-05-31 21:10:24 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,21 @@
+<div class="modal-dialog" role="document">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<h5 class="modal-title" id="ModalLabel">Confirm Logout</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			  <span aria-hidden="true">&times;</span>
+			</button>
+		  </div>
+		  <div class="modal-body">
+			Are you sure you want to Logout?
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+			<button type="button" class="btn btn-primary">Confirm</button><li><a href="login.php"></a></li>
+			
+
+		  </div>
+		</div>
+	  </div>
+	</div>`;
+        
